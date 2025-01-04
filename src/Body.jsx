ea6a098c87e029c3c16ea9b1238378df3a211b0a
ErikAJensen <111729359+ErikAJensen@users.noreply.github.com>
2025-01-04 19:30:59 +0100

    Lagt til Button

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,17 +1,21 @@
 function Body() {
   return (
-    <article>
-      <p>
-        I dette prosjektet skal jeg fokusere på å lære meg grunnleggende og
-        teoretisk kunnskap om React for å kunne ta skriftlig eksamen. Jeg vil
-        øve på ulike funksjoner og syntakser.
-      </p>
+    <body>
+      <article>
+        <p>
+          I dette prosjektet skal jeg fokusere på å lære meg grunnleggende og
+          teoretisk kunnskap om React for å kunne ta skriftlig eksamen. Jeg vil
+          øve på ulike funksjoner og syntakser.
+        </p>
 
-      <p>
-        Jeg skal fordele opp nettsiden i forksjellige komponenter, slik at jeg
-        kan øve på forksjellige deler av React.
-      </p>
-    </article>
+        <p>
+          Jeg skal fordele opp nettsiden i forksjellige komponenter, slik at jeg
+          kan øve på forksjellige deler av React.
+        </p>
+
+        <button>HandleSearch</button>
+      </article>
+    </body>
   );
 }
 
