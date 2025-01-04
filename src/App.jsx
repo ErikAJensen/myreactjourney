1f04875f0d12ff475118ab438efc876211c91907
ErikAJensen <111729359+ErikAJensen@users.noreply.github.com>
2025-01-04 19:24:11 +0100

    Tekst
    Legger inn beskrivelse

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Header from "./Header";
+import Body from "./Body";
+import "./Styles/Main.scss";
 import "./App.css";
 
 function App() {
-  return <>{Header()}</>;
+  return (
+    <>
+      {Header()}
+
+      {Body()}
+    </>
+  );
 }
 
 export default App;
---
+++ src/Body.jsx
@@ -0,0 +1,18 @@
+function Body() {
+  return (
+    <article>
+      <p>
+        I dette prosjektet skal jeg fokusere på å lære meg grunnleggende og
+        teoretisk kunnskap om React for å kunne ta skriftlig eksamen. Jeg vil
+        øve på ulike funksjoner og syntakser.
+      </p>
+
+      <p>
+        Jeg skal fordele opp nettsiden i forksjellige komponenter, slik at jeg
+        kan øve på forksjellige deler av React.
+      </p>
+    </article>
+  );
+}
+
+export default Body;
