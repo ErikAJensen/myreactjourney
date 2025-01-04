5653b4b1f9ee4068118ae410e216353125cd78ef
ErikAJensen <111729359+ErikAJensen@users.noreply.github.com>
2025-01-04 19:13:44 +0100

    Setter opp en enkel react side
    Har fjernet medfulgt innhold, inkluderer overskrift

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div>
+      <h1>
+        Dette en nettside som skal brukes for å øve til Eksamen i ITM30617-1 25V
+        Utvikling av interaktive nettsteder
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
