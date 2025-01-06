d4b0aa7bdfa6cb186a35ff88d09d9c5d3c121378
Erik0631 <111729359+ErikAJensen@users.noreply.github.com>
2025-01-06 12:19:37 +0100

    små jobbing med Route
    startet å sette opp linker

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Header from "./Header";
 import Body from "./Body";
+import useeffect from "./UseEffect";
 import "./Styles/Main.scss";
 import "./App.css";
 
 function App() {
   return (
+    
     <>
+    <Router>
+      <Routes>
+        <Route patch="/" element={<Body/>}></Route>
+      </Routes>
+    </Router>
+
+
       {Header()}
+
+      {useeffect()}
 
       {Body()}
     </>
---
+++ src/UseEffect.jsx
@@ -0,0 +1,9 @@
+function useeffect () {
+    return (
+        <div>
+            <button>Useeffect</button>
+        </div>
+    )
+}
+
+export default useeffect
