73ac6f88f8d25aad7cef60db604bf7d215a7309a
ErikAJensen <111729359+ErikAJensen@users.noreply.github.com>
2025-01-07 19:35:41 +0100

    Update Props.jsx

--- src/Components/Props.jsx
+++ src/Components/Props.jsx
@@ -4,6 +4,7 @@ function Student(props) {
   return (
     <div className="student-card">
       <p>Name: {props.name}</p>
+      <p>Last Name: {props.lastname} </p>
       <p>Age: {props.age}</p>
       <p>Student: {props.isStudent ? "Yes" : "No"}</p>
     </div>
@@ -14,8 +15,8 @@ function StudentsPage() {
   return (
     <div>
       <h1>Students</h1>
-      <Student name="Erik" age={25} isStudent="Yes" />
-      <Student name="Bjørn" age={40} isStudent="No" />
+      <Student name="Erik" lastname="Aas" age={25} isStudent="Yes" />
+      <Student name="Bjørn" lastname="Jensen" age={40} isStudent="No" />
     </div>
   );
 }
