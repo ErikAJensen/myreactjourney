5a81e275f34e8fbef97b55e21862f2f623e8fa54
ErikAJensen <111729359+ErikAJensen@users.noreply.github.com>
2025-02-17 18:50:11 +0100

    Update Body.jsx

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,7 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function Body() {
-  return <body></body>;
+  return (
+    <div>
+      <article>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+        temporibus a numquam! Laudantium nihil perspiciatis mollitia quia
+        recusandae dolores labore aliquam reprehenderit et suscipit aliquid
+        harum, dolore saepe officiis aut!
+      </article>
+    </div>
+  );
 }
 
 export default Body;
